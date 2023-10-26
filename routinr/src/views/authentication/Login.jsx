d6682b7eb1e2ff3fb3fa9df949782e50e8240676
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-10-26 12:53:21 +0100

    ..

--- routinr/src/views/authentication/Login.jsx
+++ routinr/src/views/authentication/Login.jsx
@@ -1,30 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import SubmitBtn from "../../assets/icons/signup.png";
+
 
 const Login = () => {
 
   return (
-    <div className="w-[100%] h-screen bg-cover bg-center bg-mobilebg bg-black flex justify-center pt-10">
-      <div className="w-[90%] md:w-[40%] h-fit  bg-opacity-20 bg-blur-lg bg-white rounded-lg shadow-lg">
+    <div className="main w-full h-[100vh] grid place-items-center">
+      <div className="md:w-1/3 custom-sm:w-11/12 bg-black shadow-2xl rounded-2xl p-5 bg-clip-padding bg-opacity-25">
         <div className="p-5">
-          <h1 className="text-white text-4xl ">
-            Welcome <br /> Back Kiane
+          <h1 className="my-[20px] text-white text-3xl font-semibold">
+            Welcome Back Kiane
           </h1>
-          <div className="mt-20">
+          <form action="" className="flex flex-col">
             <input
               type="text"
+              name=""
+              id=""
               placeholder="Email"
-              className="bg-white bg-opacity-40 bg-blur-lg  rounded-md py-3 px-4 block w-full mt-3 text-white placeholder:text-white"
+              className="input px-3 text-white bg-black shadow-xl rounded-md bg-clip-padding bg-opacity-25"
             />
             <input
               type="password"
+              name=""
+              id=""
               placeholder="Password"
-              className="bg-white bg-opacity-40 bg-blur-lg  rounded-md py-3 px-4 block w-full mt-3 text-white placeholder:text-white"
+              className="input px-3 text-white bg-black shadow-xl rounded-md bg-clip-padding bg-opacity-25"
             />
-          </div>
+          </form>
           <div className="flex justify-between text-white mt-20">
             <h1 className="text-[30px]">Sign In</h1>
-            <img src="../../assets/rou.png" alt="arrow" />
+            <button type="submit" className="">
+              <img
+                src={SubmitBtn}
+                alt="Submit"
+                className="w-[40px] h-[40px] float-right ml-2"
+              />
+            </button>
           </div>
           <div className="flex justify-between text-white mt-10">
             <Link to={"/signup"}>
