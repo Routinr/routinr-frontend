c238eafae6f14372f7c07257653dbcc4270a186d
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-10-26 12:33:46 +0100

    add bg

--- routinr/tailwind.config.js
+++ routinr/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     ],
   
   theme: {
-    extend: {},
+    extend: {
+       backgroundImage: {
+        'mobilebg': "url('./src/assets/bg.png')",
+      },
+    },
   },
   plugins: [],
 }
