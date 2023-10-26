6973d43195d2d02c08200e66441276c7828ff3d5
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-10-26 12:34:01 +0100

    ..

--- routinr/src/App.jsx
+++ routinr/src/App.jsx
@@ -6,12 +6,16 @@ import './index.css'
 
 // importing routes for other pages
 import HomePage from "./views/HomePage";
+import Login from "./views/authentication/Login";
+import Signup from "./views/authentication/Signup";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<HomePage />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/signup" exact element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
--- routinr/src/views/authentication/Login.jsx
+++ routinr/src/views/authentication/Login.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
   return (
-    <div>
-      
+    <div className="w-[100%] h-screen bg-cover bg-center bg-mobilebg bg-black flex justify-center pt-10">
+      <div className="w-[90%] md:w-[40%] h-fit  bg-opacity-20 bg-blur-lg bg-white rounded-lg shadow-lg">
+        <div className="p-5">
+          <h1 className="text-white text-4xl ">
+            Welcome <br /> Back Kiane
+          </h1>
+          <div className="mt-20">
+            <input
+              type="text"
+              placeholder="Email"
+              className="bg-white bg-opacity-40 bg-blur-lg  rounded-md py-3 px-4 block w-full mt-3 text-white placeholder:text-white"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="bg-white bg-opacity-40 bg-blur-lg  rounded-md py-3 px-4 block w-full mt-3 text-white placeholder:text-white"
+            />
+          </div>
+          <div className="flex justify-between text-white mt-20">
+            <h1 className="text-[30px]">Sign In</h1>
+            <img src="../../assets/rou.png" alt="arrow" />
+          </div>
+          <div className="flex justify-between text-white mt-10">
+            <Link to={"/signup"}>
+              <h1 className="text-md underline">Sign Up</h1>
+            </Link>
+            <Link to={"/forgotpassword"}>
+              <h1 className="text-md underline">Forgot Password?</h1>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login
