a966791c6fae6a8c4f9874ddeb84664b8230a32b
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-10-26 12:23:18 +0100

    signup initial

--- routinr/src/App.jsx
+++ routinr/src/App.jsx
@@ -5,13 +5,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css'
 
 // importing routes for other pages
-import HomePage from "./views/HomePage";
+import HomePage from "./views/HomePage.jsx";
+import Signup from "./views/authentication/Signup.jsx";
+import Login from "./views/authentication/Login.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<HomePage />} />
+        <Route path="signup" exact element={<Signup />} />
+        <Route path="login" exact element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- routinr/tailwind.config.js
+++ routinr/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     ],
   
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'custom-sm': '300px',
+      }
+    },
   },
   plugins: [],
 }
