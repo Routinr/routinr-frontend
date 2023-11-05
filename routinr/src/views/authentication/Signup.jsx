636ac0dcd3bb3d2e57912c5849de725e23059c3a
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-11-05 21:51:21 +0100

    next commit

--- routinr/src/views/authentication/Signup.jsx
+++ routinr/src/views/authentication/Signup.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../css/App.css";
 import SubmitBtn from "../../assets/icons/signup.png";
-import DesktopAuth from "../../assets/images/auth/DesktopAuth.png"
-
+import DesktopAuth from "../../assets/images/auth/DesktopAuth.png";
 
 const Signup = () => {
   return (
     <div className="main w-full h-[100vh] grid place-items-center">
-      <div className=" flex 
-    md:pr-0 md:py-0 md:w-7/12 md:justify-between md:items-center
-    bg-black shadow-2xl rounded-2xl p-5 bg-clip-padding bg-opacity-25">
-      <div className="w-[50%] h-[60%] grid items-center">
+      <div
+        className="auth-cont flex
+        w-2/3
+        lg:pr-0 lg:py-0 lg:w-7/12 lg:justify-between lg:items-center
+      bg-black shadow-2xl rounded-2xl p-5 bg-clip-padding bg-opacity-25"
+      >
+        <div className="w-[95%] lg:w-[50%] lg:h-[60%] grid items-center">
           <h1 className="my-[20px] text-white text-3xl font-semibold text-center">
             Create account
           </h1>
@@ -54,7 +56,7 @@ const Signup = () => {
             </button>
           </form>
 
-          <div className="flex flex-start w-full">
+          <div className="flex flex-start w-full mb-5">
             <Link
               to="/login"
               className="text-white mx-2 mt-3 font-semibold underline text-[17px]"
@@ -64,8 +66,11 @@ const Signup = () => {
           </div>
         </div>
 
-      <img src={DesktopAuth} alt="" className='desktop-img h-[500px] ml-[35px]'/>
-
+        <img
+          src={DesktopAuth}
+          alt=""
+          className="desktop-img flex h-[500px] ml-[35px]"
+        />
       </div>
     </div>
   );
