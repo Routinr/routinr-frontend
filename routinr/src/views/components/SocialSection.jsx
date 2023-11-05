2737da148f4e2b7d8b6e5f8927e76dd054f32490
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-11-05 13:51:42 +0100

    react fast marquee

--- routinr/src/views/components/SocialSection.jsx
+++ routinr/src/views/components/SocialSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Marquee from "react-fast-marquee";
+import Marquee from "react-fast-marquee"
 import socialicon from "../../assets/icons/socialicon.png";
 import socialicon1 from "../../assets/icons/socialicon1.png";
 
