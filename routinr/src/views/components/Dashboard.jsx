c84e008249ff312d433620c270d1b58e70b110eb
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-11-07 17:08:02 +0100

    dashboard page

--- routinr/src/views/components/Dashboard.jsx
+++ routinr/src/views/components/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import DashboardLayout from "../layout/DashboardLayout";
+import Topbar from "./Topbar";
 
 const Dashboard = () => {
   return (
     <DashboardLayout>
-    <div className="bg-red-500 h-screen">Hii</div>
+      <div className="h-[90vh] bg-white shadow-2xl rounded-2xl bg-opacity-5 w-[80%] p-5 mt-5 ">
+        <Topbar />
+        <div className="">welcome to dashboard page</div>
+      </div>
     </DashboardLayout>
   );
 };
