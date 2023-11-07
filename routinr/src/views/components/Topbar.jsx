abadc6205ae78c66f401e720056c81a8458e9d98
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-11-07 17:09:24 +0100

    topbar page

--- routinr/src/views/components/Topbar.jsx
+++ routinr/src/views/components/Topbar.jsx
@@ -1,11 +1,13 @@
 import React from "react"
+import avather from "../../assets/icons/avather.png";
 
 const Topbar= () => {
-    return(
-        <div className="bg-green-500">
-            Topbar
-        </div>
-    )
+    return (
+      <div className="flex items-center text-white gap-5">
+        <img className="w-[50px] h-[50px]" src={avather} alt="task" />
+        <h1>Hi Alex!</h1>
+      </div>
+    );
 }
 
 export default Topbar
