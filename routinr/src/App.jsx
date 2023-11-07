23403bcff178560a4b24bae89328b3dfe473bde7
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-11-07 17:06:21 +0100

    add dashoard pages route

--- routinr/src/App.jsx
+++ routinr/src/App.jsx
@@ -9,6 +9,10 @@ import HomePage from "./views/HomePage.jsx";
 import Signup from "./views/authentication/Signup.jsx";
 import Login from "./views/authentication/Login.jsx";
 import Dashboard from './views/components/Dashboard';
+import HelpCenter from './views/components/HelpCenter.jsx';
+import Notes from './views/components/Notes.jsx';
+import Sethings from './views/components/Sethings.jsx';
+import Tasks from './views/components/Tasks.jsx';
 
 const App = () => {
   return (
@@ -18,6 +22,10 @@ const App = () => {
         <Route path="/login" exact element={<Login />} />
         <Route path="/signup" exact element={<Signup />} />
         <Route path="/dashboard" exact element={<Dashboard />} />
+        <Route path="/helpcenter" exact element={<HelpCenter />} />
+        <Route path="/notes" exact element={<Notes />} />
+        <Route path="/sethings" exact element={<Sethings />} />
+        <Route path="/tasks" exact element={<Tasks />} />
       </Routes>
     </BrowserRouter>
   );
