9d75603035b8e8b0bacbf0c49caec8ab178db4c2
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-11-07 17:08:41 +0100

    sethings page

---
+++ routinr/src/views/components/Sethings.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import DashboardLayout from '../layout/DashboardLayout';
+import Topbar from './Topbar';
+
+const Sethings = () => {
+  return (
+    <DashboardLayout>
+      <div className="h-[90vh] bg-white shadow-2xl rounded-2xl bg-opacity-5 w-[80%] p-5 mt-5 ">
+        <Topbar />
+        <div className="">welcome to sethings page</div>
+      </div>
+    </DashboardLayout>
+  );
+}
+
+export default Sethings
