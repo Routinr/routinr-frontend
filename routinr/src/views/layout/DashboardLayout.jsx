7edc9aae9277d4f627092b5af5b0f73643891a3d
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-11-11 12:25:09 +0100

    remove react-responsive ...

--- routinr/src/views/layout/DashboardLayout.jsx
+++ routinr/src/views/layout/DashboardLayout.jsx
@@ -1,32 +1,27 @@
 import React, { useState } from "react";
-import { useMediaQuery } from "react-responsive";
 import Sidebar from "../components/Sidebar";
 import MobileTopbar from "../components/MobileTopbar";
+import MobileSidebar from "../components/MobileSidebar";
 const DashboardLayout = ({children }) => {
-  const isMobile = useMediaQuery({ maxWidth: 768 });
    const [isSidebarVisible, setIsSidebarVisible] = useState(true);
    const toggleSidebar = () => {
-     if (isMobile) {
-       setIsSidebarVisible(!isSidebarVisible);
-     }
+     setIsSidebarVisible(!isSidebarVisible);
    };
   return (
-    <div>
-      {isMobile && (
-        <div className="bg-[#031A3E] md:hidden p-5">
-          <MobileTopbar toggleSidebar={toggleSidebar} />
+    <div className="bg-[#031A3E]">
+      <div className="bg-[#031A3E] md:hidden p-5">
+        <MobileTopbar toggleSidebar={toggleSidebar} />
+      </div>
+      {isSidebarVisible && (
+        <div className="fixed right-[10px] z-30 md:hidden">
+            <MobileSidebar />
         </div>
       )}
-      {isSidebarVisible && isMobile && (
-        <div className=" w-[20%] h-screen fixed left-0 bg-[#031A3E] z-20">
-          <Sidebar />
-        </div>
-      )}
-      <div className="hidden md:block w-[20%] h-screen fixed left-0 bg-[#031A3E] z-20">
-          <Sidebar />
-        </div>
-      <div className="w-full md:w-[80%] fixed md:ml-[20%] bg-[#031A3E] z-10 ">
-        <div className="h-screen">{children}</div>
+      <div className="hidden md:block w-[20%] h-screen fixed left-0 bg-[#031A3E] z-10">
+        <Sidebar />
+      </div>
+      <div className="w-full md:w-[80%] md:ml-[20%] bg-[#031A3E] z-0 p-5 ">
+        <div className="h-screen flex justify-center">{children}</div>
       </div>
     </div>
   );
