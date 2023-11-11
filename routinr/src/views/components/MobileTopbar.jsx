7779090818f65c1188e5938b22e2e3e3fdd514b7
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-11-11 12:23:29 +0100

    mobile topbar

--- routinr/src/views/components/MobileTopbar.jsx
+++ routinr/src/views/components/MobileTopbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import avather from "../../assets/icons/avather.png";
+import hi from "../../assets/icons/hi.png";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const MobileTopbar = ({ toggleSidebar }) => {
   return (
     <div className="flex flex-row justify-between items-center text-white gap-5">
-      <div className="flex items-center gap-3">
+      <div className="flex items-center text-white gap-3">
         <img className="w-[50px] h-[50px]" src={avather} alt="task" />
         <h1>Hi Alex!</h1>
+        <img className="w-[20px] h-[20px]" src={hi} alt="hi" />
       </div>
       <div>
         <GiHamburgerMenu
