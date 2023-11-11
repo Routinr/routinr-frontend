fd22148e30951e27d347c5c3e1766da3b1b47693
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-11-11 12:24:12 +0100

    add hi icon

--- routinr/src/views/components/Topbar.jsx
+++ routinr/src/views/components/Topbar.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import avather from "../../assets/icons/avather.png";
+import hi from "../../assets/icons/hi.png";
+
 
 const Topbar= () => {
     return (
-      <div className="flex items-center text-white gap-5">
+      <div className="flex items-center text-white gap-3">
         <img className="w-[50px] h-[50px]" src={avather} alt="task" />
         <h1>Hi Alex!</h1>
+        <img className="w-[20px] h-[20px]" src={hi} alt="hi" />
       </div>
     );
 }
