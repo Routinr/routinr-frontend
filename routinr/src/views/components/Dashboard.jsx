5090ac02a0dc259546712eba83383c30482b219f
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-11-13 16:51:13 +0100

    ..

--- routinr/src/views/components/Dashboard.jsx
+++ routinr/src/views/components/Dashboard.jsx
@@ -21,6 +21,39 @@ const Dashboard = () => {
             alt="water glass cup"
           />
         </div>
+        <h1 className="mt-5 text-white font-bold">Morning Routine</h1>
+        <div className="md:w-[400px] lg:w-[500px] w-full h-[300px] border-white border-[1px] rounded-md p-5 mt-5">
+          <div className="flex justify-between items-center">
+            <div className="rounded-md w-[200px] md:w-[400px] bg-gray-600 text-white p-2">
+              <li>Go For A 10 Mins Jog</li>
+            </div>
+            <div className="w-[40px] h-[40px] bg-gray-600 rounded-md"></div>
+          </div>
+          <div className="flex justify-between items-center pt-3">
+            <div className="rounded-md w-[200px] md:w-[400px] bg-gray-600 text-white p-2">
+              <li>Use Your Sunscreen</li>
+            </div>
+            <div className="w-[40px] h-[40px] bg-gray-600 rounded-md"></div>
+          </div>
+          <div className="flex justify-between items-center pt-3">
+            <div className="rounded-md w-[200px] md:w-[400px] bg-gray-600 text-white p-2">
+              <li>Your Slim Waist Pilates</li>
+            </div>
+            <div className="w-[40px] h-[40px] bg-gray-600 rounded-md"></div>
+          </div>
+          <div className="flex justify-between items-center pt-3">
+            <div className="rounded-md w-[200px] md:w-[400px] bg-gray-600 text-white p-2">
+              <li>Cook Your Breakfast</li>
+            </div>
+            <div className="w-[40px] h-[40px] bg-gray-600 rounded-md"></div>
+          </div>
+          <div className="flex justify-between items-center pt-3">
+            <div className="rounded-md w-[200px] md:w-[400px] bg-gray-600 text-white p-2">
+              <li>Feed Your Pet</li>
+            </div>
+            <div className="w-[40px] h-[40px] bg-gray-600 rounded-md"></div>
+          </div>
+        </div>
       </div>
     </DashboardLayout>
   );
