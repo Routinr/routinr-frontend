d09829caf1b66938a4a8690dcae87f8d6623aeb3
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-11-15 15:57:07 +0100

    adjustments and styling

--- routinr/src/views/components/Dashboard.jsx
+++ routinr/src/views/components/Dashboard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import DashboardLayout from "../layout/DashboardLayout";
 import water from "../../assets/icons/water.png";
+import "../../css/App.css"
 import Topbar from "./Topbar";
 
 const Dashboard = () => {
   return (
     <DashboardLayout>
-      <div className="h-[100vh] md:bg-white md:shadow-2xl md:rounded-2xl md:bg-opacity-5 w-full md:w-[80%] md:p-5  z-20 border-[0px] md:border-[1px] border-white border-opacity-10">
+      <div className="dashboard-cont h-[90vh] md:bg-white md:shadow-2xl md:rounded-2xl md:bg-opacity-5 w-full md:w-[80%] md:p-5  z-20 border-[0px] md:border-[1px] border-white border-opacity-10 overflow-scroll">
         <div className="hidden md:block">
           <Topbar />
         </div>
